df69602f2189e59047e2ee22caf70385efa82b07
abenteuerzeit <amroz.muzyka@gmail.com>
2022-11-02 22:31:29 +0100

    Disable default account verification

--- VLC/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
+++ VLC/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -2,15 +2,15 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
+using System;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace VLC.Areas.Identity.Pages.Account
 {
@@ -60,7 +60,7 @@ namespace VLC.Areas.Identity.Pages.Account
 
             Email = email;
             // Once you add a real email sender, you should remove this code that lets you confirm the account
-            DisplayConfirmAccountLink = true;
+            DisplayConfirmAccountLink = false;
             if (DisplayConfirmAccountLink)
             {
                 var userId = await _userManager.GetUserIdAsync(user);
