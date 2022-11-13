1e1e49a5925d5c886bcd1904b7e456d829d064e9
KasiaPiciu <peterpalacz12@gmail.com>
2022-11-13 21:33:56 +0100

    style: change some style

--- VLC/Models/MealManager/MealManager.cs
+++ VLC/Models/MealManager/MealManager.cs
@@ -22,11 +22,10 @@ namespace VLC.Models.MealManager
         [Required, Display(Name = "Total Calories"), BindProperty]
         public int TotalCalories { get; set; }
             
-
         [Required, Display(Name = "Number of meals"), BindProperty]
         public int MealCount { get; set; }
 
-        [Required, Display(Name = "Diet Preference"), BindProperty, ]
+        [Required, Display(Name = "Diet Preference"), BindProperty]
         public Diets Diet { get; set; }
         //public Diets Diet { get; set; } = Diets.Whatever;
 
@@ -64,7 +63,7 @@ namespace VLC.Models.MealManager
 
         public int CountTotalCalories()
         {
-            return (int)((10 * Weight) + (6.25 * Height) - (5 * Age) + 5);
+            return (int)(10 * Weight + 6.25 * Height - 5 * Age + 5);
         }
     }
 }
